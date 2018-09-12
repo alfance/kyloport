1062acd2b91389aaed999f668fd64938260db0d5
Kylo Xue <jiao_xue@hotmail.com>
2018-09-11 20:26:52 -0400

    update package

---
+++ js/views/ProjectView/components/Table.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default class Table extends React.Component {
+  render () {
+    return (
+      <div className='project-title-layout project-title-cycnav'>
+        <div className='project-title-text'>
+          <div className='cycnav-logo project-title-logo' />
+          <h3> {this.props.content.title}</h3>
+        </div>
+      </div>
+    )
+  }
+}
