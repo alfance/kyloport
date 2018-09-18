776ff4f4cc535da7d085087f6dfa94f6cf452991
alfance <j4xue@ryerson.ca>
2018-09-17 20:32:04 -0400

    adding helper

--- js/views/ProjectView/components/LayoutText.js
+++ js/views/ProjectView/components/LayoutText.js
@@ -1,24 +1,13 @@
 import React from 'react'
-
+import ComponentHelp from './ComponentHelp'
 export default class LayoutText extends React.Component {
   render () {
       const {title, content, className} = this.props.content
-      let listContent = {}
-
-      if(content.constructor === Array){
-        listContent = content.map(list => {
-            return (
-              <li className={className} key={list}>{list}</li>
-            )
-          })
-      } else {
-          listContent = <p>{content}</p>
-      }
 
     return (
       <div>
           <h3>{title}</h3>
-          {listContent}
+          
       </div>
     )
   }
