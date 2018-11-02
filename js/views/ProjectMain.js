fe0ae6dd5400e677fee01449cf497248f372f917
Kylo Xue <jiao_xue@hotmail.com>
2018-11-01 20:39:42 -0400

    intro

--- js/views/ProjectMain.js
+++ js/views/ProjectMain.js
@@ -4,15 +4,14 @@ import PortfolioLayout from '../components/Portfolio/PortfolioLayout'
 class ProjectMain extends React.Component {
   render () {
     return (
-      <div>
-        <div className='main-intro'>
-            <div className='project-title-layout designer-intro' >
-          <h1> Hello, Kylo Xue here. </h1>
-          <p>I'm a product designer with a background in digital media. <br />
-          UX / UI  <br />
-      I am also in the progress to progressively expand my T zone into Interaction design and front-end deveopment.
-            </p>
-          </div>
+      <div className='main-intro'>
+        <div className='project-title-layout designer-intro' >
+          <h1>Hi, I am Kylo Xue </h1>
+          <hr className='main-hr' />
+          <h4>I'm an experience & interaction designer who loves building digital products. </h4>
+          <h4>As a problem solver and storyteller, I believe and embrace the principles of user-centred design and human-computer interaction. Over the past 4 years, I have delivered projects in mobile & web app, IOT, game, and 3D with the focus of delighted user journey.</h4>
+          <p>When I am not designing, I enjoy spending time pave my path </p>
+          <p />
         </div>
         <PortfolioLayout />
       </div>
