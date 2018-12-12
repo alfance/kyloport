5a2ee8812b349a8b6824021d02ebbce0d93c9497
Kylo Xue <jiao_xue@hotmail.com>
2018-12-11 22:06:38 -0500

    change resume

--- js/views/Resume.js
+++ js/views/Resume.js
@@ -2,20 +2,20 @@ import React from 'react'
 import LayoutImage from './ProjectView/components/LayoutImage'
 
 const resumeImage = {
-    title:'Kylo Xue resume',
+  title: 'Kylo Xue resume',
   img: '../../../assets/images/kylo_Xue_resume.png',
-  content:[
-      '**Download PDF Here**',
-      'For detailed resume, <a href =https://www.linkedin.com/in/kyloxue/>**Visit my Linkedin**</a>',
+  content: [
+    '__[Download PDF Here](../../../assets/images/kylo_Xue_resume.png)__',
+    'For detailed resume, __[Visit my Linkedin](https://www.linkedin.com/in/kyloxue/)__'
   ]
 }
 
 class Resume extends React.Component {
   render () {
     return (
-        <div className='project-content-layout'>
-            <LayoutImage content={resumeImage} />
-        </div>
+      <div className='project-content-layout'>
+        <LayoutImage content={resumeImage} />
+      </div>
     )
   }
 }
