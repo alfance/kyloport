20f7d8a002ae13520a293a0b5a96aa96eca461ad
alfance <jiao_xue@hotmail.com>
2019-10-02 11:49:14 -0400

    joist draft done

--- js/views/ProjectView/Joist.js
+++ js/views/ProjectView/Joist.js
@@ -22,7 +22,7 @@ const titleText1 = {title: '1) Research & empathy'}
 
 const titleText3 = {title: '2) Explore the Solution'}
 
-const titleText4 = {title: 'Case Study on the Development of Sections'}
+const titleText4 = {title: 'Case Study on One of the Job Winning Features'}
 
 const titleText5 = {title: 'Reflections'}
 
@@ -107,15 +107,16 @@ const threeSolutions = {
 }
 
 const sectiontest = {
+  title:'Test out the designs',
   sectionClass: 'project-layout-wide',
   content: 'The same approach is used to work out the best solution, we implemented in-app notification to ask volunteers to try out the design variations. ',
-  img: '../../../assets/images/proj_joist/section_wireframe_1.png',
-  label: 'Early stage wireframe exploring multiple approaches for adding sections to categorize line items in each documents.'
+  img: '../../../assets/images/proj_joist/test-flow.png',
+  label: '**Test flow**: users would see a notification asking to test out the prototype. After they have agreed to the terms, we randomly assign the a variation and track the progress they have made.'
 }
 
 const heatmap = {
   sectionClass: 'project-layout-wide',
-  content: 'For the section feature, we run into the difficulty of coming up with a design that can be used by all types of contractors. What we end up doing, test it out.',
+  content: 'We have sent out the test to around 400 users. Out of the 90 users who completed all the tasks, we were able to track their clicks, events and flow together with the users who did not make it to the end. Following is a heatmap showing the clicks on each screen from one of the variants. ',
   img: '../../../assets/images/proj_joist/section_heatmap.png'
 }
 const evaluation4 = {
@@ -123,85 +124,81 @@ const evaluation4 = {
   sectionClass: 'project-layout-wide',
   content: 'Again the users had told us ',
   columns: [{
-    Header: <b>Questions</b>,
+    Header: '',
     accessor: 'questions' // String-based value accessors!
   }, {
-    Header: <b>Prototype Deisgn 1</b>,
+    Header: <b>Prototype 1</b>,
     accessor: 'user1'
   },{
-    Header: <b>Prototype Deisgn 2</b> ,
+    Header: <b>Prototype 2</b> ,
     accessor: 'user2'
   },{
-    Header: <b>Prototype Deisgn 3</b> ,
+    Header: <b>Prototype 3</b> ,
     accessor: 'user3'
   }],
   data: [{
-    questions: <b>% of user transition from in-app promo message</b>,
-    user1: '8%',
-    user2: '15%',
-    user3:''
+    questions: <b>% Success Rate</b>,
+    user1: '29%👎',
+    user2: <b>58%👍</b>,
+    user3:'45%'
   }, {
-    questions: <b>% transition from first intro banner</b>,
-    user1: '41%',
-    user2: '69%',
-    user3:''
+    questions: <b>Average Duration</b>,
+    user1: <b>59s👍</b>,
+    user2: '89s',
+    user3:'183s👎'
   }, {
-    questions: <b>% Plan selected</b>,
-    user1: '10%',
-    user2: '20%',
-    user3:''
-  },{
-    questions: <b>Number of feedbacks</b>,
-    user1: '2',
-    user2: '11',
-    user3:''
+    questions: <b>Test Coverage</b>,
+    user1: <b>48%👍</b>,
+    user2: '33%👎',
+    user3:'39%'
   }]
+}
+
+const chart2 = {
+  img: '../../../assets/images/proj_joist/chart2.svg',
+  label:'**Chart**: Do the users who completed the tasks understand the purpose & functionality of the prototype?'
 }
 const idea1cons = {
   content: ['**PROS**: this is a dynamic approach to generating more freedom and flexibility for the users. The hovercard design is also subtle enough that it does not obscure much of the information on the original page. ',
  '**CONS**: The real estate space is very limited. Certain information has been sacrificed due to a lower priority.  ']
 }
-const interaction1 = {
-  title: 'Crafting the Interaction',
+
+const ideacombine = {
+  title: 'Combining the best cases to form a new solution',
   deviceFrame: 'phone',
-  contentLeft: 'While the user profile pages vary in content, layout and style. I discovered that they all share the same elements: user profile image, user name, short description and occationally links. For Ideation 2, I want to propose a **Consistent header section** for all user profile pages that allows visitors to explore different contents.',
+  content: ['Both design 1 and design 2 have the potential to be the solution for creating sections. However, there are concerns associated with each of the prototypes as well. After engaging a couple of conversations with the product team, I combined the two prototypes to create a new solution.'],
+  imgLeft: '../../../assets/images/proj_joist/combine1.png',
+  imgMiddle: '../../../assets/images/proj_joist/combine2.png',
+  imgRight: '../../../assets/images/proj_joist/combine13.png',
+  contentLeft: '**Step 1**: Users have the freedom to toggle section on or off.',
+  contentMiddle: '**Step 2**: Once section is on, users must start with creating section.',
+  contentRight: '**Step 3**: Creating line items is enabled after section is been created.'
+}
+
+const interaction1 = {
+  deviceFrame: 'phone',
+  contentLeft: 'The new solution combines the advantages of design 1 and 2 by providing very clear guidance in the flow and enough amount of flexibility and freedom for all users. All stakeholders feel confident to implement this new approach with the supporting data. ',
   videoRight: '../../../assets/images/proj_joist/section_design3.mp4',
 }
 
-const ideacombine = {
-  title: 'Combining ideas',
-  sectionClass: 'project-layout-wide',
-  content: 'Due to the close results and feedbacks from the users, we ran into the conflict of selecting the design.',
-  img: '../../../assets/images/proj_joist/Homepage-Hero-Backgroun.png',
+const finalComments = {
+  sectionClass:'remove-title-sapce',
+  content:['#### * "Would be a great feature to be able to divide into sections for each room." * ####',
+'#### * "Great added feature. This would make my estimates more detailed." * ####',
+'#### * "Nice! Will be very helpful!" * ####']
 }
-
-const prototypeApp = {
-  title: 'Design Mockup',
-  deviceFrame: 'laptop',
-  sectionClass: 'project-layout-wide',
-  content: 'The following design mockup is for the forum page. The UI design follows the general style of the Unity live platform.',
-  img: '../../../assets/images/proj_joist/features-estimate.png',
-}
-const prototypeApp2 = {
-  deviceFrame: 'desktop',
-  content: 'Another design with more detailed information about the user on the Unity Connect main page.',
-  img: '../../../assets/images/proj_joist/Homepage-Hero-Backgroun.png',
-}
-
-const prototypeApp3 = {
-  deviceFrame: 'laptop',
-  sectionClass: 'project-layout-wide',
-  content: 'The visual style of the Header section contains consistency with the overall Unity scheme and palette. No matter which community page it is, the header remain unchanged.',
-  imgLeft: '../../../assets/images/proj_joist/Joist-Invoicing-Mobile-Device-Screen.png',
-  contentLeft:'Community Answer page',
-  imgRight: '../../../assets/images/proj_joist/features-estimate.png',
-  contentRight:'Unity Connect'
-}
-
-const video = {
-  title:'Video Walkthrough',
-  img: '../../../assets/images/proj_joist/dummy-plan3.png',
-}
+const reflections = [
+  { title: 'Learning',
+    subject: 'Invole your users in any design & research stage.',
+    learn: 'As a designer, it is almost a no brainer to always think to form the perspective of your users. But a lot of times we have neglected the significance of interacting with our users throughout the design cycle. Getting real feedback from the users can often reveal areas we have missed.'
+  }, {
+    subject: 'Usability test in the natural habitat',
+    learn: 'The designer or researcher does not always have to be present to test out prototypes in the users&#39;t natural habitat. By creating a scenario similar to the user&#39;t digital environment can also generate authentic results. '
+  }, {
+    subject: 'If you have doubts, clear the doubt before starting any design work.',
+    learn: 'The first 2 steps of interactive design thinking are to help the designer understand the problem and needs. Utilizing the methodologies to raise your understanding is crucial to the contribution of the final product.'
+  }
+]
 
 class Joist extends React.Component {
   render () {
@@ -209,7 +206,7 @@ class Joist extends React.Component {
       title: 'A Job-Winning Solutions for Contractors',
       headerImage: '../../assets/images/proj_joist/joist-logo-wht.svg',
       logo: '../../assets/images/proj_joist/unitylogo-w.svg',
-      color: '#484ADF',
+      color: '#67C118',
       url: '',
       textonColor: 'text-dark'
     }
@@ -242,14 +239,13 @@ class Joist extends React.Component {
           <LayoutImage content={sectiontest} />
           <LayoutImage content={heatmap} />
           <TableComponent content={evaluation4} />
+          <LayoutImage content={chart2} />
           <FindingComponent content={idea1cons} style={style} />
+          <LayoutThree content={ideacombine} />
           <LayoutHalf content={interaction1} />
-          <LayoutImage content={ideacombine} />
-          <LayoutImage content={prototypeApp} />
-          <LayoutImage content={prototypeApp2} />
-          <LayoutHalf content={prototypeApp3} />
-          <LayoutImage content={video} />
 
+          <Problem content={finalComments} style={style} />
+          <Reflection content={reflections} />
           <ProjectFooter content={style} style={style} />
         </div>
       </div>
