e2cd75656a9aebcf1cc2fb720da34da710ab0815
alfance <jiao_xue@hotmail.com>
2020-03-23 02:38:23 -0400

    add animated device

--- js/views/ProjectMain.js
+++ js/views/ProjectMain.js
@@ -9,6 +9,22 @@ class ProjectMain extends React.Component {
   render () {
     return (
       <div className='main-intro'>
+      <div className='device-animation'>
+        <div className='device-animation-mac'>
+          <img className='device-animation-shadow' src='../../../assets/images/devices/mac_shadow.svg' />
+          <img src='../../../assets/images/devices/clay_mac.png' />
+        </div>
+
+        <div className='device-animation-ipad'>
+          <img className='device-animation-shadow' src='../../../assets/images/devices/ipad_shadow.svg' />
+          <img src='../../../assets/images/devices/clay_ipad.png' />
+        </div>
+
+        <div className='device-animation-iphone'>
+          <img className='device-animation-shadow' src='../../../assets/images/devices/iphone_shadow.svg' />
+          <img src='../../../assets/images/devices/clay_iphone.png' />
+        </div>
+      </div>
         <div className='project-title-layout designer-intro' >
           <Spring from={{ opacity: 0, marginTop: 200 }} to={{opacity: 1, marginTop: 0}}>
             {({ opacity, marginTop }) =>
